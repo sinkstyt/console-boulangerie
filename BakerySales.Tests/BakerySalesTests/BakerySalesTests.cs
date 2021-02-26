0466300e142a802e8a8746b81bb92ece9c0dd7e2
Tyler Sinks <tyler.sinks@gmail.com>
2021-02-26 11:56:26 -0500

    compose and pass Bread constructor spec

--- BakerySales.Tests/BakerySalesTests/BakerySalesTests.cs
+++ BakerySales.Tests/BakerySalesTests/BakerySalesTests.cs
@@ -1,5 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using BakerySales;
+using BakerySales.Models;
 
 namespace BakerySales.Tests
 {
@@ -7,13 +7,24 @@ namespace BakerySales.Tests
   public class BakerySalesTests
   {
     [TestMethod]
-    public void Bread_ReturnsAnInstanceOfBread_Bread()
+    public void BreadConstructor_InstantiatesBreadInstance_Bread()
     {
       // Arrange
+      string newType = "rye";
+      int countOfType = 2;
 
       // Act
-
+      Bread newBread = new Bread(newType, countOfType);
+      
       // Assert
+      Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
+
+    // [TestMethod]
+    // public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+    // {
+    //   // any necessary logic to prep for test; instantiating new classes, etc.
+    //   Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+    // }
   }
 }
