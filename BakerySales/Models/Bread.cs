dd2a2af5f02faa0acf2b09c5ed393e7e9f8f8b5d
Tyler Sinks <tyler.sinks@gmail.com>
2021-02-26 12:46:24 -0500

    add _allThings Lists, ClearAll methods

--- BakerySales/Models/Bread.cs
+++ BakerySales/Models/Bread.cs
@@ -1,14 +1,23 @@
+using System.Collections.Generic;
+
 namespace BakerySales.Models
 {
   public class Bread
   {
     public string TypeLoaf { get; set; }
     public int CountLoaf { get; set; }
+    private static List<Bread> _allLoaves = new List<Bread> { };
     
     public Bread(string type, int num)
     {
       string Type = type;
       int Count = num;
+      _allLoaves.Add(this);
+    }
+
+    public static void ClearAll()
+    {
+      _allLoaves.Clear();
     }
   }
 }
--- BakerySales/Models/Pastry.cs
+++ BakerySales/Models/Pastry.cs
@@ -1,14 +1,24 @@
+using System.Collections.Generic;
+
 namespace BakerySales.Models
 {
   public class Pastry
   {
     private string _typePastry { get; }
     public string NameLoaf { get; set; }
+    private int _price { get; }
+    private static List<Pastry> _allPastries = new List<Pastry> { };
     
     public Pastry(string typeOfPastry, int number)
     {
       string Type = typeOfPastry;
       int Count = number;
+      _allPastries.Add(this);
+    }
+
+    public static void ClearAll()
+    {
+      _allPastries.Clear();
     }
   }
 }
