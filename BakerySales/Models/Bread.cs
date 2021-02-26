dd3e72ceea8bf11de0ddf908ea9ee9faa08d2dba
Tyler Sinks <tyler.sinks@gmail.com>
2021-02-26 15:48:25 -0500

    pass getBread price spec

--- BakerySales/Models/Bread.cs
+++ BakerySales/Models/Bread.cs
@@ -6,15 +6,22 @@ namespace BakerySales.Models
   {
     public string TypeLoaf { get; set; }
     public int CountLoaf { get; set; }
+    public int Price { get; set; }
     private static List<Bread> _allLoaves = new List<Bread> { };
     
     public Bread(string type, int num)
     {
-      string Type = type;
-      int Count = num;
+      TypeLoaf = type;
+      CountLoaf = num;
       _allLoaves.Add(this);
     }
 
+    public Bread(string type, int num, int cost)
+      : this(type, num)
+    {
+      Price = cost;
+    }
+    
     public static void ClearAll()
     {
       _allLoaves.Clear();
