990373db63ade3b686670f4a0e79d54d502d0359
Tyler Sinks <tyler.sinks@gmail.com>
2021-02-26 20:02:37 -0500

    begin UI logic, add title README

--- BakerySales/Program.cs
+++ BakerySales/Program.cs
@@ -9,7 +9,7 @@ class Program : Product
   public string breadDiscount = "Buy 2 loaves and receive an additional for free";
   public string pastryDiscount = "For every three pastries that you buy, Pierre discounts your order by a whole dollar";
 
-  public string Greet()
+  public void Greet()
   {
     Console.WriteLine(WelcomeToShop);
     Console.WriteLine(loavesPrice);
@@ -17,12 +17,51 @@ class Program : Product
     Console.WriteLine(specialsAnnounce);
     Console.WriteLine(breadDiscount);
     Console.WriteLine(pastryDiscount);
-    return WelcomeToShop;
   }
 
+  public int offerBreadProduct(string typeProduct, string countWanted)
+  {
+    Console.WriteLine("Would you like to purchase bread? [yes or no]");
+    try
+    {
+      int numLoaves = int.Parse(Console.ReadLine());
+    }
+    catch (System.Exception)
+    {
+      Console.WriteLine("Message = {0}", ex.Message);  
+      throw;
+    }
+        Console.WriteLine("Would you like to purchase bread? [yes or no]");
+
+    Console.WriteLine("How many loaves would you like?")
+    try
+    {
+      int numLoaves = int.Parse(Console.ReadLine());
+    }
+    catch (System.Exception)
+    {
+      Console.WriteLine("Message = {0}", ex.Message);  
+      throw;
+    }
+    Console.WriteLine("How many loaves would you like?")
+  }
 
   static void Main()
   {
-    Console.WriteLine();
+    Greet();
+
+    Console.WriteLine("Would you like to add pastries to your order? [yes or no]");
+    
+    Console.WriteLine("How many loaves would you like?")
+    try
+    {
+      int numLoaves = int.Parse(Console.ReadLine());
+    }
+    catch (System.Exception)
+    {
+      Console.WriteLine("Message = {0}", ex.Message + "is what our system is telling us. Please enter the number of loaves of bread desired:");
+
+      throw;
+    }
   }
 }
