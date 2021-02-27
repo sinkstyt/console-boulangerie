f48f0d7813254e8df88fb08514852ffc4673f393
Tyler Sinks <tyler.sinks@gmail.com>
2021-02-26 19:22:09 -0500

    remove Console.WriteLines from Bread.cs

--- BakerySales/Models/Bread.cs
+++ BakerySales/Models/Bread.cs
@@ -45,14 +45,11 @@ namespace BakerySales.Models
 
     public static int CalcAdjNumBreads(int countPerDiscount, int bonusLoaves)
     {
-      Console.WriteLine("totalBreadNum is: {0}", Bread.AllBreadInstancesCount());
       int totalBreadNum = Bread.AllBreadInstancesCount();
       if (totalBreadNum >= 2)
       {
         decimal loafBonusCount = Math.Floor(Convert.ToDecimal(totalBreadNum / countPerDiscount));
-        Console.WriteLine("loafBonusCount value is {0}", loafBonusCount);
         int bonus = Convert.ToInt32(loafBonusCount);
-        Console.WriteLine("bonus [loaves] int currently holds: {0}", bonus);
         totalBreadNum += Convert.ToInt32(bonus);
         return totalBreadNum;
       }
